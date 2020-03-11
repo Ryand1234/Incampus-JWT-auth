c7189167e84bd6ef23bd6d9fa646a686e195505e
Rajat Upadhyay <43850934+rajatmw1999@users.noreply.github.com>
2020-03-11 15:56:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get('/',function(req, res){
 res.send('Hi');
 });
 
-app.listen(3000, function(){
+app.listen(3000 || process.env.PORT, function(){
   console.log('Server is running on port 3000');
 });
 
